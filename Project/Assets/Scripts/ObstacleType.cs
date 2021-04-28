b1733542c59a173a3180da24b71ee33c2d43c82e
Omkar Joshi <65808188+OmkarJ13@users.noreply.github.com>
2021-04-28 15:22:42 +0530

    Added More Obstacle Types

--- Project/Assets/Scripts/ObstacleType.cs
+++ Project/Assets/Scripts/ObstacleType.cs
@@ -2,5 +2,11 @@ public enum ObstacleType
 {
     Left = 0,
     Center = 1,
-    Right = 2
+    Right = 2,
+    LeftSingle = 3,
+    CenterSingle = 4,
+    RightSingle = 5,
+    LeftOpen = 6,
+    CenterOpen = 7,
+    RightOpen = 8
 }
