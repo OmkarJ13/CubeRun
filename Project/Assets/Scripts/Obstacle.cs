95fa02e364b8061bef1e249fe6ce12145d42c0cd
Omkar Joshi <65808188+OmkarJ13@users.noreply.github.com>
2021-05-01 19:18:08 +0530

    Fixed a Bug In The Obstacle Script Where Direction To The Player Wouldn't Get Calculated

--- Project/Assets/Scripts/Obstacle.cs
+++ Project/Assets/Scripts/Obstacle.cs
@@ -5,22 +5,23 @@ public class Obstacle : MonoBehaviour
     public ObstacleType Type => type;
 
     [SerializeField] private ObstacleType type;
-    [SerializeField] private Transform player;
     [SerializeField] private float spawnDistance;
 
     private LevelManager levelManager;
+    private Transform player;
 
     private void Awake()
     {
         levelManager = FindObjectOfType<LevelManager>();
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     private void LateUpdate()
     {
-        Vector3 direction = player.transform.position - transform.position;
-        float distance = direction.z;
+        Vector3 direction = player.position - transform.position;
 
-        if (distance >= spawnDistance)
+        if (direction.z >= spawnDistance)
         {
             if (levelManager && levelManager.isActiveAndEnabled)
             {
